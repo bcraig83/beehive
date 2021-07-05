50d9701bf7a977c086c40273aebe3701e37684f0
Ben Craig <bcraig83@gmail.com>
2021-07-05 11:29:39 +0100

    Add a service for retrieve the data and then storing it in the repository.

---
+++ Beehive.Api/Core/Services/DrumService.cs
@@ -0,0 +1,40 @@
+﻿using Beehive.Api.Core.Models.Domain;
+using Beehive.Api.Infrastructure.Clients;
+using Beehive.Api.Infrastructure.Repositories;
+
+namespace Beehive.Api.Core.Services
+{
+    public class DrumService : IDrumService
+    {
+        private readonly IDrumClient _client;
+        private readonly IRepository<Drum> _repository;
+
+        public DrumService(IRepository<Drum> repository, IDrumClient client)
+        {
+            _repository = repository;
+            _client = client;
+        }
+
+        public GetDrumsResponseDto Get(GetDrumsQueryDto query)
+        {
+            var result = new GetDrumsResponseDto();
+
+            var responseFromApi = _client.GetDrumsForWarehouse(query.WarehouseNumber);
+            foreach (var drum in responseFromApi)
+            {
+                _repository.Add(drum);
+
+                var drumResponse = new DrumDto
+                {
+                    Label = drum.Label,
+                    Size = drum.Size,
+                    WarehouseNumber = drum.WarehouseNumber
+                };
+
+                result.Drums.Add(drumResponse);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Beehive.Api/Core/Services/IDrumService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Beehive.Api.Core.Models.Domain;
+
+namespace Beehive.Api.Core.Services
+{
+    public interface IDrumService
+    {
+        public GetDrumsResponseDto Get(GetDrumsQueryDto query);
+    }
+
+    public class GetDrumsQueryDto
+    {
+        public int WarehouseNumber { get; set; }
+    }
+
+    public class GetDrumsResponseDto
+    {
+        public IList<DrumDto> Drums { get; set; } = new List<DrumDto>();
+    }
+
+    public class DrumDto
+    {
+        public Size Size { get; set; }
+        public int WarehouseNumber { get; set; }
+        public string Label { get; set; }
+    }
+}
