2e6558ce27ab8dd17e16651a4cb8b36a0fbc299a
Ben Craig <bcraig83@gmail.com>
2021-07-05 11:29:14 +0100

    Create a simple DB context for drums (WIP)

---
+++ Beehive.Api/Infrastructure/Repositories/DrumDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Beehive.Api.Core.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beehive.Api.Infrastructure.Repositories
+{
+    public class DrumDbContext : DbContext
+    {
+        public DrumDbContext(DbContextOptions<DrumDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Drum> Drums { get; set; }
+    }
+}
