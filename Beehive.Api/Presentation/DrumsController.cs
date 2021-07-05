8a295965302487cea649f44e86ac3b14b4e8a49c
Ben Craig <bcraig83@gmail.com>
2021-07-05 11:29:48 +0100

    Add a drums controller (and some tidyup)

---
+++ Beehive.Api/Presentation/DrumsController.cs
@@ -0,0 +1,26 @@
+﻿using Beehive.Api.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beehive.Api.Presentation
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DrumsController : ControllerBase
+    {
+        private readonly IDrumService _service;
+
+        public DrumsController(IDrumService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{warehouseNumber:int}")]
+        public ActionResult Get(int warehouseNumber)
+        {
+            var query = new GetDrumsQueryDto {WarehouseNumber = warehouseNumber};
+            var result = _service.Get(query);
+
+            return Ok(result);
+        }
+    }
+}
--- Beehive.Api/Presentation/WeatherForecastController.cs
+++ Beehive.Api/Presentation/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Beehive.Api.Controllers
+namespace Beehive.Api.Presentation
 {
     [ApiController]
     [Route("[controller]")]
