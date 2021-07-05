67de07d87e3d41609f4f6852fc97f77c1e5ebff4
Ben Craig <bcraig83@gmail.com>
2021-07-05 11:28:02 +0100

    Add a drum client that simulates calling out to some API.

---
+++ Beehive.Api/Infrastructure/Clients/DrumClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Beehive.Api.Core.Models;
+using Beehive.Api.Core.Models.Domain;
+
+namespace Beehive.Api.Infrastructure.Clients
+{
+    public class DrumClient : IDrumClient
+    {
+        public IEnumerable<Drum> GetDrumsForWarehouse(int warehouseNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Beehive.Api/Infrastructure/Clients/IDrumClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Beehive.Api.Core.Models;
+using Beehive.Api.Core.Models.Domain;
+
+namespace Beehive.Api.Infrastructure.Clients
+{
+    public interface IDrumClient
+    {
+        public IEnumerable<Drum> GetDrumsForWarehouse(int warehouseNumber);
+    }
+}
