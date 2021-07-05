b6dc9a1fc227d97aa6503e445a0626261f5b20d8
Ben Craig <bcraig83@gmail.com>
2021-07-05 14:35:20 +0100

    Implement a test showcasing how we can get an item under test.

---
+++ Beehive.Api.Test/DrumServiceBehaviouralTest.cs
@@ -0,0 +1,24 @@
+﻿using Beehive.Api.Core.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace Beehive.Api.Test
+{
+    [Collection("Behavioural Tests")]
+    public class DrumServiceBehaviouralTest
+    {
+        private readonly Fixture _fixture;
+
+        public DrumServiceBehaviouralTest(Fixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void ShouldBeAbleToRetrieveTheItemUnderTest()
+        {
+            var itemUnderTest = _fixture.GetItemUnderTest<IDrumService>();
+            itemUnderTest.Should().NotBeNull();
+        }
+    }
+}
