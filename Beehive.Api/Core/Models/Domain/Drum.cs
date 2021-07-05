65276952002d7c04d9bc9bf789ae4f72da7650aa
Ben Craig <bcraig83@gmail.com>
2021-07-05 11:27:44 +0100

    Create drum domain object.

---
+++ Beehive.Api/Core/Models/Domain/Drum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Beehive.Api.Core.Models.Domain
+{
+    public class Drum
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public Size Size { get; set; } = Size.Medium;
+        public string Label { get; set; }
+        public int WarehouseNumber { get; set; } = 1;
+    }
+
+    public enum Size
+    {
+        Large,
+        Medium,
+        Small
+    }
+
+
+}
