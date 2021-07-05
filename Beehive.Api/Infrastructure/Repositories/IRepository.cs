243f7ec8348a09a8407e32e87bb9793577e361f5
Ben Craig <bcraig83@gmail.com>
2021-07-05 11:29:02 +0100

    Add generic repository.

---
+++ Beehive.Api/Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Beehive.Api.Infrastructure.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IQueryable<T> GetAll();
+        IQueryable<T> Query(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ Beehive.Api/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beehive.Api.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            _dbSet.Add(entity);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return Query(x => true);
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>> expression)
+        {
+            // Need to add code to handle "EntityNotFoundException"
+            return _dbSet.Where(expression);
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity != null) _dbSet.Remove(entity);
+        }
+    }
+}
