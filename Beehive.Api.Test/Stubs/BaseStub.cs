fdbd277d3822cd21dba0848da681a7a1af4f5e41
Ben Craig <bcraig83@gmail.com>
2021-08-19 10:08:25 +0100

    Add an example of a more sophisticated stub/spy implementation.

---
+++ Beehive.Api.Test/Stubs/BaseStub.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Beehive.Api.Test.Stubs
+{
+    public abstract class BaseStub<TKey, TRequest, TResponse>
+    {
+        protected readonly IDictionary<TKey, TResponse> Expectations = new Dictionary<TKey, TResponse>();
+        protected readonly IList<TRequest> RecordedRequests = new List<TRequest>();
+
+        public void ClearRecordedItems()
+        {
+            RecordedRequests.Clear();
+        }
+
+        public IList<TRequest> GetRecordedRequests()
+        {
+            return RecordedRequests;
+        }
+
+        public void AddExpectation(TKey key, TResponse response)
+        {
+            Expectations.Add(key, response);
+        }
+
+        public void ClearExpectations()
+        {
+            Expectations.Clear();
+        }
+    }
+}
---
+++ Beehive.Api.Test/Stubs/DrumStub.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Beehive.Api.Core.Interfaces.Clients;
+using Beehive.Api.Core.Models;
+
+namespace Beehive.Api.Test.Stubs
+{
+    public class DrumStub : BaseStub<int, int, IEnumerable<Drum>>, IDrumClient
+    {
+        public IEnumerable<Drum> GetDrumsForWarehouse(int warehouseNumber)
+        {
+            RecordedRequests.Add(warehouseNumber);
+
+            var hasResponse = Expectations.TryGetValue(warehouseNumber, out var response);
+            return hasResponse ? response : new List<Drum>();
+        }
+    }
+}
